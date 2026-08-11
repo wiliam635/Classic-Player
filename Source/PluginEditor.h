41c8d37ac07e231906aa07aa3e88ed540f317ea1
wiliam635 <tecladoonlineoficial@gmail.com>
2026-08-11 11:03:23 -0300

    Include chord display style definition in editor

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -3,6 +3,7 @@
 #include <juce_gui_extra/juce_gui_extra.h>
 #include <juce_audio_utils/juce_audio_utils.h>
 #include "PluginProcessor.h"
+#include "ChordDetector.h"
 #include <array>
 #include <atomic>
 
